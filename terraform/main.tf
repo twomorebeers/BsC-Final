f30d63966390881c84db177d49f978087e7df30a
twomorebeers <rutasbogdan@gmail.com>
2026-05-14 00:28:13 +0300

    Improved CLI capabilities.

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,12 +4,13 @@ terraform {
 	required_providers {
 		proxmox = {
 			source  = "Telmate/proxmox"
-			version = "~> 3.0"
+			version = "3.0.2-rc07"
 		}
 	}
 }
 
 provider "proxmox" {
+	// pm_tls_insecure is true by default for self-signed labs
 	pm_api_url          = var.proxmox_api_url
 	pm_api_token_id     = var.proxmox_api_token_id
 	pm_api_token_secret = var.proxmox_api_token_secret
